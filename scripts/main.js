f7baba6a58e6ff63a51fd9ed8e5cc85d138c17f9
kamhix <kimly02@gmail.com>
2015-09-18 17:27:52 +0100

    Removed the custom redirectToGoogle that is no longer necessary and that is causing the csrf missing bug.

--- scripts/main.js
+++ scripts/main.js
@@ -42,56 +42,25 @@ view.addEventListener('load', function () {
     loader.className = 'loader';
   };
 
-  winFrame.redirectToGoogle = function () {
-    var state = {
-        "success_page": "",
-        "csrf": "b8ed60fe23914a3d9564e1d57a65d666",
-        "inline": 0,
-        "connect": 0
-    };
-
-    state = encodeURIComponent(JSON.stringify(state));
-
-    var origin = winFrame.location.origin;
-
-    if(!origin) {
-        origin = location.protocol + "//" + winFrame.location.hostname + (winFrame.location.port ? ':' + winFrame.location.port: '');
-    }
-
-    var redirect_uri = origin + '/Users/gplusRedirect';
-
-    var href = 'https://accounts.google.com/o/oauth2/auth?scope=' +
-               'email%20profile&' +
-               'state='+ state +'&' +
-               'redirect_uri='+ redirect_uri +'&'+
-               'response_type=token&' +
-               'apppackagename=com.todoist&' +
-               'client_id=600979030768-7o35cjq1gv138e22oj0j39u0bp0mn3hj.apps.googleusercontent.com';
-
-    winFrame.popup(href, 'Google Authentication', 450, 550);
-
-    return false;
-  }
-
   winFrame.popup = function (url, title, w, h) {
 
-      var oldHref = window.location.href;
+    var oldHref = window.location.href;
 
-      var win = gui.Window.open(url, {
-        toolbar: false,
-        position: 'center',
-        title: title,
-        width: w,
-        height: h
-      });
+    var win = gui.Window.open(url, {
+      toolbar: false,
+      position: 'center',
+      title: title,
+      width: w,
+      height: h
+    });
 
-      win.on('close', function() {
-        this.hide();
-        this.close();
-        window.location.href = oldHref;
-      });
+    win.on('close', function() {
+      this.hide();
+      this.close();
+      window.location.href = oldHref;
+    });
 
-      return win;
+    return win;
   }
 
   redirectLink(winFrame);

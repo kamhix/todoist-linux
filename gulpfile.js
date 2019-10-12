606ce190077220e431cf713fcc4cce1c9fdfb67a
Hakim Ouake <hakimouake@gmail.com>
2019-10-12 15:44:57 +0100

    Updated dev dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ var tap = require('gulp-tap');
 var gzip = require('gulp-gzip');
 var chmod = require('gulp-chmod');
 
-gulp.task('clean', run('rm -rf dist'));
+gulp.task('clean', gulp.series(run('rm -rf dist')));
 
-gulp.task('build', ['clean'], 
-  run('build --tasks linux-x86,linux-x64 --mirror https://dl.nwjs.io/ .'));
+gulp.task('build', gulp.series(['clean'], 
+  run('build --tasks linux-x86,linux-x64 --mirror https://dl.nwjs.io/ .')));
 
-gulp.task('chmod', ['build'], function () {
+gulp.task('chmod', gulp.series(['build'], function () {
   gulp.src('dist/*/todoist')
   .pipe(tap(function(file, t) {
     // gulp.src('dist/' + path.basename(file.path) + '/**/*')
@@ -21,9 +21,9 @@ gulp.task('chmod', ['build'], function () {
   }))
     // .pipe(chmod(0o755))
     // .pipe(gulp.dest('dist'))
-});
+}));
 
-gulp.task('tar', ['chmod'], function () {
+gulp.task('tar', gulp.series(['chmod'], function () {
   gulp.src('dist/*')
     .pipe(tap(function(file, t) {
       gulp.src('dist/' + path.basename(file.path) + '/**/*')
@@ -31,12 +31,12 @@ gulp.task('tar', ['chmod'], function () {
         .pipe(gzip())
         .pipe(gulp.dest('tar'));
     }))
-});
+}));
 
-gulp.task('package', ['tar']);
+gulp.task('package', gulp.series(['tar']));
 
-gulp.task('start:x86', run('run --x86 --mirror https://dl.nwjs.io/ .'));
+gulp.task('start:x86', gulp.series(run('run --x86 --mirror https://dl.nwjs.io/ .')));
 
-gulp.task('start:x64', run('run --x64 --mirror https://dl.nwjs.io/ .'));
+gulp.task('start:x64', gulp.series(run('run --x64 --mirror https://dl.nwjs.io/ .')));
 
-gulp.task('start', ['start:x64']);
+gulp.task('start', gulp.series(['start:x64']));
